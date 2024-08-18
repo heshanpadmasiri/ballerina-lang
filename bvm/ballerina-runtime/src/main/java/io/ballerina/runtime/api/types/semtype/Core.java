a4e7c4dc916f24ab8b1a8bd679021d57e9ace283
Heshan Padmasiri <heshanp@wso2.com>
2024-08-18 07:26:50 +0530

    Factor out BCellSubType

--- bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/api/types/semtype/Core.java
+++ bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/api/types/semtype/Core.java
@@ -56,8 +56,8 @@ import static io.ballerina.runtime.api.types.semtype.Builder.cellContaining;
 import static io.ballerina.runtime.api.types.semtype.Builder.listType;
 import static io.ballerina.runtime.api.types.semtype.Builder.undef;
 import static io.ballerina.runtime.api.types.semtype.CellAtomicType.CellMutability.CELL_MUT_NONE;
+import static io.ballerina.runtime.api.types.semtype.CellAtomicType.cellAtomType;
 import static io.ballerina.runtime.api.types.semtype.CellAtomicType.intersectCellAtomicType;
-import static io.ballerina.runtime.internal.types.semtype.BCellSubType.cellAtomType;
 import static io.ballerina.runtime.internal.types.semtype.BListSubType.bddListMemberTypeInnerVal;
 import static io.ballerina.runtime.internal.types.semtype.BMappingProj.mappingMemberTypeInner;
 
