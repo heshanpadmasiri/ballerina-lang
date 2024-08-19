222da58d1df96275ea606e3d4c7bb268cf096a88
Heshan Padmasiri <heshanp@wso2.com>
2024-08-19 08:36:53 +0530

    Use a simple weak map for dependency manager

--- bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/types/BFiniteType.java
+++ bvm/ballerina-runtime/src/main/java/io/ballerina/runtime/internal/types/BFiniteType.java
@@ -203,7 +203,20 @@ public class BFiniteType extends BType implements FiniteType {
             return false;
         }
         BFiniteType that = (BFiniteType) o;
-        return this.valueSpace.size() == that.valueSpace.size() && this.valueSpace.containsAll(that.valueSpace);
+        if (this.valueSpace.size() != that.valueSpace.size()) {
+            return false;
+        }
+        for (var each : this.valueSpace) {
+            try {
+                if (!that.valueSpace.contains(each)) {
+                    return false;
+                }
+            } catch (NullPointerException ex) {
+                // If one of the sets is an immutable collection this can happen
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
